f096cd163366964f6477ad98af25d35281906171
ilkerdurmaz <ilker.prime@gmail.com>
2022-09-29 16:34:10 +0300

    white spaces deleted

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,44 @@
 import "./Main.css";
-import ToDoInput from '../ToDoInput/ToDoInput';
-import ToDo from '../ToDo/ToDo';
-import { BsGithub, BsLinkedin } from 'react-icons/bs';
-import { useSelector } from 'react-redux';
-
+import ToDoInput from "../ToDoInput/ToDoInput";
+import ToDo from "../ToDo/ToDo";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 export default function Main() {
-  const todos = useSelector((state) => state.todoList.list)
+  const todos = useSelector((state) => state.todoList.list);
 
   return (
     <div className="main">
       <div id="Header">
         <h2>To - Do List</h2>
         <div id="Links">
-          <a href="https://github.com/ilkerdurmaz" target="_blank" rel="noreferrer"><BsGithub /></a>
-          <a href="https://www.linkedin.com/in/ilker-durmaz" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+          <a
+            href="https://github.com/ilkerdurmaz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ilker-durmaz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsLinkedin />
+          </a>
         </div>
       </div>
       <hr />
       <ToDoInput />
       <hr />
       <div id="toDoList">
-        {todos.length > 0 ? todos.map((todo, index) => {
-          return <ToDo todo={todo} key={index} index={index} />;
-        }) : <h3>List is empty.</h3>}
+        {todos.length > 0 ? (
+          todos.map((todo, index) => {
+            return <ToDo todo={todo} key={index} index={index} />;
+          })
+        ) : (
+          <h3>List is empty.</h3>
+        )}
       </div>
     </div>
   );
--- src/todoSlice.js
+++ src/todoSlice.js
@@ -1,26 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    list:localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
-}
+  list: localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [],
+};
 
 export const todoSlice = createSlice({
-name:"todo",
-initialState,
-reducers:{
-    addTodo:(state,action)=>{
-        state.list.push(action.payload);
-        localStorage.setItem("todos",JSON.stringify(state.list));
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.list.push(action.payload);
+      localStorage.setItem("todos", JSON.stringify(state.list));
     },
-    removeTodo:(state,action)=>{ state.list=state.list.filter((_,index)=>index!==action.payload)},
-    updateTodo:(state,action)=>{
-        console.log(action.payload)
-        state.list = state.list.map((todo,index)=>index===action.payload.index?action.payload.todo:todo);
-        localStorage.setItem("todos",JSON.stringify(state.list));
-    }
-}
-})
+    removeTodo: (state, action) => {
+      state.list = state.list.filter((_, index) => index !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      console.log(action.payload);
+      state.list = state.list.map((todo, index) =>
+        index === action.payload.index ? action.payload.todo : todo
+      );
+      localStorage.setItem("todos", JSON.stringify(state.list));
+    },
+  },
+});
 
-export const {addTodo, removeTodo,updateTodo} = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
